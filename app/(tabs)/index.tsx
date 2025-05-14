3d2721f7a9ae00673dc04905456c7ba8b2af8104
Krish Teckchandani <krish2005tech@gmail.com>
2025-05-14 19:21:34 +0530

    margin of graph finetuned

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -705,7 +705,7 @@ const fetchAndResetHeaders = async () => {
                 r: '5',
               },
             }}
-            style={{ marginVertical: 8,borderRadius: 20, alignSelf: "center", paddingTop: 20,borderWidth: 0, borderColor: "#000",marginLeft: 30 }}
+            style={{ marginVertical: 8,borderRadius: 20, alignSelf: "center", paddingTop: 20,borderWidth: 0, borderColor: "#000",marginLeft: 40 }}
             bezier
             withDots={true}
             withInnerLines={false}
